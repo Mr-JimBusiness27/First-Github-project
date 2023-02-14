a91094c015b8be8263b92b77b0802c1a293d6a60
Jacob <jgra891@wgu.edu>
2023-02-13 21:46:48 -0500

    if/else/else-if statements

--- JS crash course/app.js
+++ JS crash course/app.js
@@ -1,3 +1,12 @@
-let bool = '1' ===1
+let subscribed = false
+let loggedIn = true
 
-console.log(bool)
+if (subscribed === true) {
+    console.log('show the video')
+}
+else if (loggedIn === true) {
+    console.log ('tell the user to upgrade their subscription')
+}
+else {
+    console.log('tell user to log into account')
+}
